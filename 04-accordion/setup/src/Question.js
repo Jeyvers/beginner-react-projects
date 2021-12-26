835325f9ab0455fc7b6734c5969a4a729dcfe813
Jeyi Adole <erthnice@gmail.com>
2021-12-26 19:16:08 +0100

    Tutor's correction (accordion challenge)

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ title, info }) => {
-  const [readMore, setReadMore] = useState(false);
+  const [toggleInfo, setToggleInfo] = useState(false);
   return (
     <article className='question'>
       <header>
         <h4>{title} </h4>
-        <button className='btn' onClick={() => setReadMore(!readMore)}>
-          {readMore ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        <button className='btn' onClick={() => setToggleInfo(!toggleInfo)}>
+          {toggleInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
 
-      <p>{readMore ? info : ``}</p>
+      {toggleInfo && <p>{info}</p>}
     </article>
   );
 };
