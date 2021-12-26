9e4a64ee4b2ae38a25dba4b6db0df09472a9b669
Jeyi Adole <erthnice@gmail.com>
2021-12-26 13:26:33 +0100

    Tour Project Done

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -38,12 +38,13 @@ function App() {
       </main>
     );
   }
+
   if (tours.length === 0) {
     return (
       <main>
         <div className='title'>
           <h2>No tours left</h2>
-          <button className='btn' onClick={() => fetchTours()}>
+          <button className='btn' onClick={fetchTours}>
             {' '}
             Refresh
           </button>
@@ -51,6 +52,7 @@ function App() {
       </main>
     );
   }
+
   return (
     <>
       <main>
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -9,16 +9,17 @@ const Tour = ({ id, image, info, price, name, removeTour }) => {
         <div className='tour-info'>
           <h4>{name}</h4>
           <h4 className='tour-price'>${price} </h4>
-          <p>
-            {readMore ? info : `${info.subString(0, 200)}...`}
-            <button onClick={() => setReadMore(!readMore)}>
-              {readMore ? 'Show less' : 'Read more'}
-            </button>
-          </p>
-          <button className='delete-btn' onClick={() => removeTour(id)}>
-            Not Interested
-          </button>
         </div>
+
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'Show less' : 'Read more'}
+          </button>
+        </p>
+        <button className='delete-btn' onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
       </footer>
     </article>
   );
