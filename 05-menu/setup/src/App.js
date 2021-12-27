9e41df842ef9148ed6a4bb79f2157983edea48e6
Jeyi Adole <erthnice@gmail.com>
2021-12-27 16:37:16 +0100

    Menu Application Done

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,9 +3,21 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className='menu section'>
@@ -13,7 +25,9 @@ function App() {
           <h2>our menu</h2>
           <div className='underline'></div>
         </div>
-      </section>{' '}
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
     </main>
   );
 }
