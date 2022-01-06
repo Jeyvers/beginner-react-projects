1775faddfb45f6af1d3a78454b68d4612c0ea04e
Jeyi Adole <erthnice@gmail.com>
2022-01-06 20:21:47 +0100

    Cocktail Application Progress

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, officia
+        blanditiis? Obcaecati perferendis ab consequatur veritatis cumque, ipsum
+        numquam minus, natus quia ea corrupti ipsa repellendus itaque optio
+        quibusdam fugiat. Voluptatum illo nisi numquam officiis nihil sequi est
+        consequuntur esse.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
